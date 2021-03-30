6a28b484522c0356f3bd0c5548ab5df3603bf567
Alex <alexzanetti99@gmail.com>
2021-03-30 17:29:32 +0200

    Updated pom.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
+        launch(args);
         System.out.println("Test");
+    }
+
+    @Override
+    public void start(Stage stage) {
+        
     }
 }
