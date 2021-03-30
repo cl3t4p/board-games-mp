586505bbf3b7132448dc8cad08cf4278e364f02b
Alex <alexzanetti99@gmail.com>
2021-03-30 17:31:27 +0200

    Update Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -9,6 +12,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        
+        Label label = new Label("Hello");
+        Scene scene = new Scene( label,400, 200);
+
+        stage.setTitle("Square Color");
+        stage.setScene(scene);
+        stage.show();
     }
 }
