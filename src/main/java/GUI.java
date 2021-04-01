ffe60db5b87a317770bbda3caacf2d7aece1541f
Lisa Stuflesser <lstuflesser@unibz.it>
2021-04-01 15:55:52 +0200

    added FX basic structure to GUI class

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,3 +1,23 @@
-public class GUI {
-    int X;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        public void start(Stage primaryStage){
+
+            Group root = new Group();
+            Scene scene = new Scene(root, 300, 120, Color.GREEN);
+
+            primaryStage.setTitle("BoardGames");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+
+        public static void main(String[] args){
+          launch(args);
+
+        }
+    }
 }
