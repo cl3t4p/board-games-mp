0667693432e6f8c88f1dd0833840ea7128bc7752
Alex <alexzanetti99@gmail.com>
2021-04-07 17:03:32 +0200

    Fixing Errors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
+import Util.ServerHandler;
 
-
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        GUI.startAPP(args);
+    public static void main(String[] args) throws IOException {
+        //GUI.startAPP(args);
+
+        ServerHandler serverHandler = new ServerHandler(5000);
+        serverHandler.start();
     }
 
 
