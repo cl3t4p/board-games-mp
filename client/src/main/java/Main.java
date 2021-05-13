fc92a259644888732aa1e0ca7f14b324642e412d
Alex <alexzanetti99@gmail.com>
2021-05-13 17:04:26 +0200

    Remove some server stuff on client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-<<<<<<< Updated upstream
-        TicTacToGUI.startAPP(args);
-=======
+
+        //TicTacToGUI.startAPP(args);
+
         MainGUI.startAPP(args);
->>>>>>> Stashed changes
+
 
        // ServerHandler serverHandler = new ServerHandler(5000);
        // serverHandler.start();
