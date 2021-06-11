7d12311561872d7fb9767f777f5fb03411d90d42
Stuflesser Lisa (Student Com20) <lisa.stuflesser@stud-inf.unibz.it>
2021-06-11 07:36:37 +0000

    Update BattelShipGUI.java

--- client/src/main/java/BattelShipGUI.java
+++ client/src/main/java/BattelShipGUI.java
@@ -10,6 +10,29 @@ import javafx.stage.Stage;
 
 public class BattelShipGUI extends Application {
 
+    public Button[][] btn = new Button[10][10];
+    public Button[][] rbtn = new Button[10][10];
+
+    public Random zufallszahl = new Random();
+
+    protected int[] ShipsPos = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+    public int[] MyShipPos = new int[100];
+
+    protected boolean wasShot;
+    protected boolean shipOnPos;
+
+    public int suchePosition(int[] feld) {
+        int pos = -1;
+        for (int i = 0; i < feld.length; i++) {
+            if (feld[i] == -1) {
+                pos = i;
+                break;
+            }
+
+        }
+        return pos;
+    }
+
     public static void startAPP(String[] args) {
         launch(args);
     }
@@ -45,95 +68,169 @@ public class BattelShipGUI extends Application {
         }
     }
 
+     public int[] placeShip(int[] feld, int pos, int x, int y) {
+        feld[pos] = 10 * x + y;
+        return feld;
+    }
+
+    public boolean Shoot(int[] feld, int x, int y) {
+        boolean shoot = false;
+        for (int k = 0; k < feld.length; k++) {
+            if (feld[k] == 10 * x + y) {
+                shoot = true;
+            }
+        }
+        return shoot;
+    }
+
+    public boolean ComputerShoot(int[] feld1, int[] feld2) {
+        boolean shoot = false;
+        for (int k = 0; k < 11; k++) {
+            if (feld1[k] == feld2[k]) {
+                shoot = true;
+            }
+        }
+        return shoot;
+    }
+
+
+        public boolean isNumberAvailable ( int[] feld, int zahl){
+            boolean isAvailable = false;
+            for (int j = 0; j < feld.length; j++) {
+                if (feld[j] == zahl) isAvailable = true;
+            }
+            return isAvailable;
+        }
+
     @Override
     public void start(Stage primaryStage) {
 
-        primaryStage.setTitle("BattleShip");
+        Label info1 = new Label();
+            Label info2 = new Label("You can place 10 Ships");
+            Label Titel1 = new Label("Enemy");
+            Label Titel2 = new Label("You");
 
-        Label info = new Label();
-        Label info2 = new Label();
+            Titel1.setStyle("-fx-font-size: 50; -fx-translate-x: 140");
+            Titel2.setStyle("-fx-font-size: 50; -fx-translate-x: 150");
 
+            int[] AIsetShips = new int[10];
 
-        for(int j=0;j<10;j++){
-            for(int i=0;i<10;i++) {
-                String buttonName = "(" + i + j + ")" ;
-                btn[i][j] = new Button(buttonName);
-                btn[i][j].setPrefSize(50, 50);
+            int t = 0;
 
+            do {
+                int neueZahl = zufallszahl.nextInt(100);
+                if (!isNumberAvailable(AIsetShips, neueZahl)) {
+                    AIsetShips[t] = neueZahl;
+                    t++;
+                }
 
-                wasShot = false;
-                schipOnPos = false;
+            } while (t < 10);
 
-                int finalI = i;
-                int finalJ = j;
-                btn[i][j].setOnAction(new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e) {
-                        if(Shoot(btn[finalI][finalJ])) {
-                            info.setText("Treffer");
-                        } else {
-                            info.setText("Daneben");
+            int[] AIShoot = new int[50];
+
+            do {
+                int neueZahl = zufallszahl.nextInt(101) + 100;
+                if (!isNumberAvailable(AIShoot, neueZahl)) {
+                    AIShoot[t] = neueZahl;
+                    t++;
+                }
+
+            } while (t < 50);
+
+            for (int j = 0; j < 10; j++) {
+                for (int i = 0; i < 10; i++) {
+
+                    String buttonName = "(" + i + j + ")";
+                    btn[i][j] = new Button(buttonName);
+                    btn[i][j].setPrefSize(40, 40);
+                    btn[i][j].setStyle("-fx-text-fill: rgba(0,0,0,0);");
+
+                    wasShot = false;
+
+                    int posI = i;
+                    int posJ = j;
+
+                    btn[i][j].setOnAction(new EventHandler<ActionEvent>() {
+                        public void handle(ActionEvent e) {
+                            if (Shoot(AIsetShips, posI, posJ)) {
+                                info1.setText("Hit");
+                                btn[posI][posJ].setStyle("-fx-background-color: red; -fx-text-fill: rgba(0,0,0,0)");
+                            } else {
+                                info1.setText("Miss");
+                                btn[posI][posJ].setStyle("-fx-background-color: blue; -fx-text-fill: rgba(0,0,0,0)");
+                            }
+                            btn[posI][posJ].setDisable(true);// = false;
+                            ComputerShoot(AIShoot,MyShipPos);
                         }
-
-                        btn[finalI][finalJ].setDisable(true);// = false;
-                    }
-                });
+                    });
+                }
             }
+
+            for (int k = 0; k < 10; k++) {
+                for (int p = 0; p < 10; p++) {
+                    String butnName = "(" + (p + 10) + k + ")";
+                    rbtn[k][p] = new Button(butnName);
+                    rbtn[k][p].setPrefSize(40, 40);
+                    rbtn[k][p].setStyle("-fx-text-fill: rgba(0,0,0,0);");
+
+                    wasShot = false;
+                    shipOnPos = false;
+
+                    int posK = k;
+                    int posP = p;
+
+                    rbtn[k][p].setOnAction(new EventHandler<ActionEvent>() {
+                        public void handle(ActionEvent e) {
+                            int z = suchePosition(ShipsPos);
+                            int ms = 0;
+                            MyShipPos[ms]= posP*10;
+                            MyShipPos[ms]= ms +posK;
+                            if (z != -1) {
+                                ShipsPos = placeShip(ShipsPos, z, posK, posP);
+                                rbtn[posK][posP].setStyle("-fx-background-color: black; -fx-text-fill: rgba(0,0,0,0)");
+                                rbtn[posK][posP].setDisable(true);
+                            } else {
+                                info2.setText("all Ships placed");
+                            }
+                        }
+                    });
+                }
+            }
+
+            GridPane leftboard = new GridPane();
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    leftboard.add(btn[i][j], i, j);
+                }
+            }
+
+            GridPane rightboard = new GridPane();
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    rightboard.add(rbtn[i][j], i, j);
+                }
+            }
+
+            GridPane board = new GridPane();
+            board.add(leftboard, 1, 2);
+            board.add(rightboard, 2, 2);
+            board.add(Titel1, 1, 1);
+            board.add(Titel2, 2, 1);
+            board.setLayoutX(20);
+            board.setLayoutY(10);
+            board.setHgap(45);
+
+            GridPane gpinfo = new GridPane();
+            gpinfo.add(info1, 1, 1);
+            gpinfo.add(info2, 2, 1);
+
+            gpinfo.setStyle("-fx-font-size: 20; -fx-translate-x: 400; -fx-translate-y: 550");
+            gpinfo.setHgap(50);
+
+            Group root = new Group(board, gpinfo);
+            Scene scene = new Scene(root, 1000, 700);
+
+            primaryStage.setTitle("BattleShip");
+            primaryStage.setScene(scene);
+            primaryStage.show();
         }
-
-        for(int j=0;j<10;j++){
-            for(int i=10;i<20;i++) {
-                String btnName = "(" + i + j + ")" ;
-                btn[i][j] = new Button(btnName);
-                btn[i][j].setPrefSize(50, 50);
-
-                wasShot = false;
-                schipOnPos = false;
-
-                int finalI = i;
-                int finalJ = j;
-                btn[i][j].setOnAction(new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e) {
-                        placeShip(btn[finalI][finalJ]);
-                        btn[finalI][finalJ].setDisable(true);// = false;
-                    }
-                });
-            }
-        }
-
-        Button exit = new Button("Exit");
-        exit.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-
-
-        GridPane board1 = new GridPane();
-        for(int i=0;i<10;i++){
-            for (int j = 0; j < 10; j++) {
-                board1.add(btn[i][j], i, j);
-            }
-        }
-
-        GridPane board2 = new GridPane();
-        for(int i=10;i<20;i++){
-            for (int j = 0; j < 10; j++) {
-                board2.add(btn[i][j], i, j);
-            }
-
-        }
-
-        board1.add(info, 10,2);
-        board1.setLayoutX(20);
-
-        board2.add(exit, 20,1);
-        board2.add(info2, 100, 2);
-        board2.setLayoutX(600);
-
-        Group root = new Group(board1, board2);
-        Scene scene = new Scene(root, 1500, 1080);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
-}
